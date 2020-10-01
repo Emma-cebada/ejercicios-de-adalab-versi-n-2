3b8658014061fbb725ae4f834f4336c9377d91d9
Emma <emma.cebada@gmail.com>
2020-10-01 19:38:50 +0200

    Add exercise 6 to repository

---
+++ modulo-2-leccion-2-ejercicio-6/main.js
@@ -0,0 +1,11 @@
+"use strick";
+// Primera forma de hacerlo
+const hi = "Hola";
+const name = "Lucia,";
+
+console.log(`${hi} ${name} encantada de conocerte.`);
+
+// Segunda forma de hacerlo
+const adalabergirl = document.querySelector(".adalabergirl");
+console.log(adalabergirl);
+adalabergirl.innerHTML = "Lucia";
