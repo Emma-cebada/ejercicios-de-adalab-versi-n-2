b9c5a7e40e6047d31b163660258110f7a9b4f894
Emma <emma.cebada@gmail.com>
2020-10-04 20:41:03 +0200

    Add age dog calculator

---
+++ modulo-2-leccion-3-ejercicio-3/main.js
@@ -0,0 +1,13 @@
+const result = "13";
+
+if (result === 13 - 13) {
+  console.log("El número es 0");
+} else if (result === 13 - 16) {
+  console.log("El número es negativo");
+} else if (result === 13 + 2) {
+  console.log("El número más 2 es mayor que 13 pero menor o igual que 20");
+} else if (result === 13 + 20) {
+  console.log("El número es mayor que 20 pero menor que 50");
+} else {
+  console.log("el número no es 123123125");
+}
