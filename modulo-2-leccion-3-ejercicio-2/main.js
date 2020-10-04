0b590343f7dccab3628fd0bfb9f86fb8dd4ce7fe
Emma <emma.cebada@gmail.com>
2020-10-04 19:36:47 +0200

    Add exercise to my repository

---
+++ modulo-2-leccion-3-ejercicio-2/main.js
@@ -0,0 +1,9 @@
+const name = "Lucia";
+
+if (name === "Lucia") {
+  console.log("¡Bievenida, Lucia!");
+} else {
+  console.log(
+    "Lo siento pero el usuario que has introducido no está registrado"
+  );
+}
