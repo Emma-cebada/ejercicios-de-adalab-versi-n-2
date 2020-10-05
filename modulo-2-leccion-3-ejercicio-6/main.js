c5f4311aca2924e73ca2ba142e032ac012806ddf
Emma <emma.cebada@gmail.com>
2020-10-05 11:13:49 +0200

    Add Bill image exercise

---
+++ modulo-2-leccion-3-ejercicio-6/main.js
@@ -0,0 +1,13 @@
+// Esto es el código que tenemos en el ejemplo y que tengo que reescribir utilizando un ternario.
+
+let avocados;
+const avocadoPrice = 1.5;
+const money = 33;
+
+// avocados(money >= avocadoPrice ? "money / avocadoPrice" : "0");
+
+// if (money >= avocadoPrice) {
+//   avocados = money / avocadoPrice;
+// } else {
+//   avocados = 0;
+// }
