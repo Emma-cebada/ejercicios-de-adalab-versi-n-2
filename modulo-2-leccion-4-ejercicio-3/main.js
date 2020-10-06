cc6f43c764c501f87fb2630eb9fe2d4073f1aa17
Emma <emma.cebada@gmail.com>
2020-10-06 10:07:35 +0200

    Add exercise 3 done in class

--- modulo-2-leccion-4-ejercicio-3/main.js
+++ modulo-2-leccion-4-ejercicio-3/main.js
@@ -1,8 +1,32 @@
 "use strict";
+// Como yo lo estaba haciendo.
+// function iva(a, b) {
+//   const result = (a / b) * 100;
+//   return result;
+// }
 
-function iva(a, b) {
-  return (a / b) * 100;
+// const amount = iva(12, 21, 100);
+// console.log("Resultado:", amount);
+
+//Hecho en clase.
+function getIva(price) {
+  const iva = price * 0.21;
+  const total = price + iva;
+  console.log(
+    `El precio es: ${price}€. El IVA es: ${iva}€. El precio total es: ${total}€.`
+  );
 }
 
-const amount = iva(12, 21);
-console.log("Resultado", amount);
+getIva(100);
+
+//Otra forma de hacerlo.
+function getIva(price) {
+  const iva = price * 0.21;
+  const total = price + iva;
+  console.log();
+  const text = `El precio es: ${price}€. El IVA es: ${iva}€. El precio total es: ${total}€.`;
+  return text;
+}
+
+const paragraph = document.querySelector(".js-text");
+paragraph.innerHTML = getIva(100);
