cb15c9d39f19c5d75dc1319e4dbeb4fecc9246a0
Emma <emma.cebada@gmail.com>
2020-10-06 22:58:21 +0200

    Add revision exercise 2

--- modulo-2-leccion-5-ejercicio-2/main.js
+++ modulo-2-leccion-5-ejercicio-2/main.js
@@ -3,11 +3,8 @@
 // elemento de HTML
 const button = document.querySelector(".button");
 
-// handler
-function showAlert(name) {
-  const name = Emma;
-  console.log(`"Hola " + ${name} + "."`);
-}
-
 // listener sobre el elemento, con tipo de evento y handler
-button.addEventListener("click", showAlert);
+button.addEventListener("click", function () {
+  const name = document.querySelector("#myInput").value;
+  console.log(`Hola ${name}`);
+});
