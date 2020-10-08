5facaddf06cdec3d1a8bbaedc6097bf3b9fcef8f
Emma <emma.cebada@gmail.com>
2020-10-08 11:31:48 +0200

    Add upgrate js code

--- modulo-2-leccion-6-ejercicio-6/main.js
+++ modulo-2-leccion-6-ejercicio-6/main.js
@@ -1,7 +1,25 @@
-const pearBasket {
+const pearBasket = {
   numMax: 10,
   numMin: 0,
-  currentNum: 1,
-  initialNum: 1,
+  currentNum: 8,
+  initialNum: 0,
+  add: function () {
+    this.currentNum = this.currentNum + 1;
+    if (this.currentNum > this.numMax) {
+      return "No se pueden meter más peras.";
+    } else {
+      return this.currentNum;
+    }
+  },
+  remove: function () {
+    return this.currentNum - 1;
+  },
+  equal: function () {
+    return (this.currentNum = this.initialNum);
+  },
 };
-
+console.log(pearBasket.add());
+console.log(pearBasket.add());
+console.log(pearBasket.add());
+// console.log(pearBasket.remove());
+// console.log(pearBasket.equal());
