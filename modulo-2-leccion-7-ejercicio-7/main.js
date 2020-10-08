0f959c1f9fca7b2ba865d883e9c0d1f7f32e042c
Emma <emma.cebada@gmail.com>
2020-10-08 21:30:21 +0200

    Add first part of exercise

--- modulo-2-leccion-7-ejercicio-7/main.js
+++ modulo-2-leccion-7-ejercicio-7/main.js
@@ -1,2 +1,35 @@
 "use strict";
 
+const adalabers = [
+  {
+    name: "María",
+    age: "29 años",
+    job: "diseñadora"
+  },
+  {
+    name: "Luciá",
+    age: "31 años",
+    job: "ingeniera química"
+  },
+  {
+  name: "Susana",
+    age: "34 años",
+    job: "periodista"
+  },
+  {
+  name: "Rocío",
+    age: "25 años",
+    job: "actriz"
+  },
+  {
+  name: "Inmaculada",
+    age: "21 años",
+    job: "diseñadora"
+  }
+];
+
+const countAdalabers = adalabers.length;
+console.log("El número de Adalabers es: " + countAdalabers);
+
+//Creo que tengo que usar el bucle for o bucle for...of
+const averageAge = ;
