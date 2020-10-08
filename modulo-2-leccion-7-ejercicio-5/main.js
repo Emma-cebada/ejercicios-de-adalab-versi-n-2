ca51fae72c8bc0507976c879d768fd12baab4f7e
Emma <emma.cebada@gmail.com>
2020-10-08 20:53:12 +0200

    Add exercise 5 unfinish

---
+++ modulo-2-leccion-7-ejercicio-5/main.js
@@ -0,0 +1,13 @@
+"use strict";
+// Parte a:
+const numbers = [7, 4, 8, 12, 23];
+let parse = parseInt(numbers);
+
+let average = 0;
+let parse2 = parseInt(average);
+
+
+for (let i = 0; i < numbers.length; i++) {
+  average += numbers[i]
+}
+console.log("La media es : " + numbers % 5);
