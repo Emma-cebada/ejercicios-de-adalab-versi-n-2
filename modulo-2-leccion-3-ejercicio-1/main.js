2cf24e0de7b76892a68fd57424ebfbfb74464df5
Emma <emma.cebada@gmail.com>
2020-10-11 20:43:43 +0200

    Add all parts of exercise

--- modulo-2-leccion-3-ejercicio-1/main.js
+++ modulo-2-leccion-3-ejercicio-1/main.js
@@ -7,3 +7,16 @@ let userAvatar = "http://www.fillmurray.com/300/300";
 
 const avatarImg = document.querySelector(".user__avatar");
 avatarImg.setAttribute("src", userAvatar || DEFAULT_AVATAR);
+
+//Vamos a suponer que el usuario cuando se registró no tenía una imagen para subir.
+
+// let userAvatar = "";
+
+//Vamos a hacer que haya una imagen sí o sí.
+if (userAvatar = "") {
+  avatarImg.setAttribute("src", userAvatar)
+} else {
+ avatarImg.setAttribute("src", DEFAULT_AVATAR)
+}
+
+
