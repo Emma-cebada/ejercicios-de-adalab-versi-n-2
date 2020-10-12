1aaf81c70b9d1c883383aac4322def75511458f7
Emma <emma.cebada@gmail.com>
2020-10-12 18:15:24 +0200

    Add html structure

--- modulo-2-leccion-3-ejercicio-6/main.js
+++ modulo-2-leccion-3-ejercicio-6/main.js
@@ -4,7 +4,8 @@ let avocados;
 const avocadoPrice = 1.5;
 const money = 33;
 
-// avocados(money >= avocadoPrice ? "money / avocadoPrice" : "0");
+avocados(money >= avocadoPrice ? "money / avocadoPrice" : "0");
+
 
 // if (money >= avocadoPrice) {
 //   avocados = money / avocadoPrice;
