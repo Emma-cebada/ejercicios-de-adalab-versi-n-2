92eee3e9add0799b04a39fd602edf4d8b8cb6525
Emma <emma.cebada@gmail.com>
2020-10-12 13:59:03 +0200

    Add another way to do it

--- modulo-2-leccion-3-ejercicio-4/main.js
+++ modulo-2-leccion-3-ejercicio-4/main.js
@@ -1,47 +1,63 @@
-const age = 2;
+// const age = 2;
 
-if (age === 1) {
+// if (age === 1) {
+//   console.log("Tu perro en edad humana tiene 15 años.");
+// } else if (age === 2) {
+//   console.log("Tu perro en edad humana tiene 24 años");
+// } else if (age === 3) {
+//   console.log("Tu perro tiene en edad humana 29 años");
+// } else if (age === 4) {
+//   console.log("Tu perro tiene en edad humana 34 años");
+// } else if (age === 5) {
+//   console.log("Tu perro tiene en edad humana 39 años");
+// } else if (age === 6) {
+//   console.log("Tu perro tiene en edad humana 44 años");
+// } else if (age === 7) {
+//   console.log("Tu perro tiene en edad humana 49 años");
+// } else if (age === 8) {
+//   console.log("Tu perro tiene en edad humana 54 años");
+// } else if (age === 9) {
+//   console.log("Tu perro tiene en edad humana 59 años");
+// } else if (age === 10) {
+//   console.log("Tu perro tiene en edad humana 64 años");
+// } else if (age === 11) {
+//   console.log("Tu perro tiene en edad humana 69 años");
+// } else if (age === 12) {
+//   console.log("Tu perro tiene en edad humana 74 años");
+// } else if (age === 13) {
+//   console.log("Tu perro tiene en edad humana 79 años");
+// } else if (age === 14) {
+//   console.log("Tu perro tiene en edad humana 84 años");
+// } else if (age === 15) {
+//   console.log("Tu perro tiene en edad humana 89 años");
+// } else if (age === 16) {
+//   console.log("Tu perro tiene en edad humana 94 años");
+// } else if (age === 17) {
+//   console.log("Tu perro tiene en edad humana 99 años");
+// } else if (age === 18) {
+//   console.log("Tu perro tiene en edad humana 104 años");
+// } else if (age === 19) {
+//   console.log("Tu perro tiene en edad humana 109 años");
+// } else if (age === 20) {
+//   console.log("Tu perro tiene en edad humana 114 años");
+// } else if (age === 21) {
+//   console.log("Tu perro tiene en edad humana 119 años");
+// } else if (age >= 22) {
+//   console.log("Tu perrito es súper viejito");
+// }
+
+
+//Otra forma de hacer la calculadora
+
+const dogAge = 6;
+const dogAge1 = 15;
+const dogAge2 = dogAge1 + 9;
+const dogAge3OrMore = ((dogAge -2) * 5) + dogAge2;
+
+if (dogAge === 1) {
   console.log("Tu perro en edad humana tiene 15 años.");
-} else if (age === 2) {
-  console.log("Tu perro en edad humana tiene 24 años");
-} else if (age === 3) {
-  console.log("Tu perro tiene en edad humana 29 años");
-} else if (age === 4) {
-  console.log("Tu perro tiene en edad humana 34 años");
-} else if (age === 5) {
-  console.log("Tu perro tiene en edad humana 39 años");
-} else if (age === 6) {
-  console.log("Tu perro tiene en edad humana 44 años");
-} else if (age === 7) {
-  console.log("Tu perro tiene en edad humana 49 años");
-} else if (age === 8) {
-  console.log("Tu perro tiene en edad humana 54 años");
-} else if (age === 9) {
-  console.log("Tu perro tiene en edad humana 59 años");
-} else if (age === 10) {
-  console.log("Tu perro tiene en edad humana 64 años");
-} else if (age === 11) {
-  console.log("Tu perro tiene en edad humana 69 años");
-} else if (age === 12) {
-  console.log("Tu perro tiene en edad humana 74 años");
-} else if (age === 13) {
-  console.log("Tu perro tiene en edad humana 79 años");
-} else if (age === 14) {
-  console.log("Tu perro tiene en edad humana 84 años");
-} else if (age === 15) {
-  console.log("Tu perro tiene en edad humana 89 años");
-} else if (age === 16) {
-  console.log("Tu perro tiene en edad humana 94 años");
-} else if (age === 17) {
-  console.log("Tu perro tiene en edad humana 99 años");
-} else if (age === 18) {
-  console.log("Tu perro tiene en edad humana 104 años");
-} else if (age === 19) {
-  console.log("Tu perro tiene en edad humana 109 años");
-} else if (age === 20) {
-  console.log("Tu perro tiene en edad humana 114 años");
-} else if (age === 21) {
-  console.log("Tu perro tiene en edad humana 119 años");
-} else if (age >= 22) {
-  console.log("Tu perrito es súper viejito");
+} else if (dogAge === 2) {
+  console.log("Tu perro en edad humana tiene " + dogAge2 + " años.");
+} else if (dogAge >=3) {
+  console.log("Tu perro en edad humana tiene " + dogAge3OrMore + " años.");
 }
