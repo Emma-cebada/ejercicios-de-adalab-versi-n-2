2c3eaa45a00ac0c36f01c2ffba726bdc0514c47d
Emma <emma.cebada@gmail.com>
2020-10-14 18:41:20 +0200

    Add resolution exercise

---
+++ modulo-2-leccion-9-ejercicio-2/main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const arr = [];
+// for (let i = 1; i <= 100; i++) {
+//   console.log('Aquí tenemos un: ' + i);
+//   arr.push(i);
+// }
+// console.log(arr);
+
+function get100Numbers(newArr) {
+  for (let i = 1; i <= 100; i++) {
+    console.log('Aquí tenemos un: ' + i);
+    newArr.push(i);
+  }
+}
+
+get100Numbers(arr);
+console.log(arr);
+
+function getReversed100Numbers(reverseArr) {
+  get100Numbers(reverseArr);
+  reverseArr.reverse();
+}
+
+getReversed100Numbers(arr);
+console.log(arr);
