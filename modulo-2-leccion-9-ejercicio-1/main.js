edac4653768fe27200f836d22e687d754d653fc8
Emma <emma.cebada@gmail.com>
2020-10-14 18:20:47 +0200

    Add resolution exercise

--- modulo-2-leccion-9-ejercicio-1/main.js
+++ modulo-2-leccion-9-ejercicio-1/main.js
@@ -1,7 +1,18 @@
 "use strict";
 
-const arr = [1, 2, 3, 4, 5];
-for ()
-  function get100Numbers() {
-    return
+const arr = [];
+// for (let i = 1; i <= 100; i++) {
+//   console.log('Aquí tenemos un: ' + i);
+//   arr.push(i);
+// }
+// console.log(arr);
+
+function get100Numbers(newArr) {
+  for (let i = 1; i <= 100; i++) {
+    console.log('Aquí tenemos un: ' + i);
+    newArr.push(i);
   }
+}
+
+get100Numbers(arr);
+console.log(arr);
