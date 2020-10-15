cf054d6473909f7afee07feb13ac933266110a58
Emma <emma.cebada@gmail.com>
2020-10-15 10:21:46 +0200

    Add second part of exercise

--- modulo-2-leccion-9-ejercicio-3/main.js
+++ modulo-2-leccion-9-ejercicio-3/main.js
@@ -2,12 +2,19 @@
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
 const arrempty = [];
+const mult3 = [];
 
 function bestLostNumber() {
   for (let i = 0; i < lostNumbers.length; i++) {
     if (lostNumbers[i] % 2 === 0) {
       arrempty.push(lostNumbers[i]);
     }
-
+    if (lostNumbers[i] % 3 === 0) {
+      mult3.push(lostNumbers[i]);
+    }
   }
 }
+
+bestLostNumber();
+console.log(arrempty);
+console.log(mult3);
